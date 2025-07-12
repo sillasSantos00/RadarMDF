581fd822097dd9f30cc788b4e5925add0fed9140
santos <sillasr61@gmail.com>
2025-07-11 21:30:06 -0300

    comite com projeto

--- src/main/java/org/example/app/database/DataBaseConnection.java
+++ src/main/java/org/example/app/database/DataBaseConnection.java
@@ -1,9 +1,20 @@
 package org.example.app.database;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DataBaseConnection {
     public static Connection connection() {
+        String url = "jdbc:mysql://localhost:3306/radarmdf";
+        String user = "root";
+        String password = "";
+        try {
+            return DriverManager.getConnection(url,user,password);
+        }catch (SQLException e) {
+            System.out.println("erro na comneccao" + e.getMessage());
 
+            return null;
+        }
     }
 }
--- src/main/java/org/example/app/view/MainApp.java
+++ src/main/java/org/example/app/view/MainApp.java
@@ -1,5 +1,6 @@
 package org.example.app.view;
 
+import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
@@ -7,12 +8,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class MainApp extends  Aplication{
+public class MainApp extends Application{
     public void start (Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/org/example/view/MainView.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Radar MDF");
-        Stage.getIcons().add(new Image("/org/example/imagens/logo.png"));
+        stage.getIcons().add(new Image("/org/example/imagens/logo.png"));
         scene.getStylesheets().add(getClass().getResource("/org/example/css/estilo.css").toExternalForm());
         stage.setScene(scene);
         stage.show();
