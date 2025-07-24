5fc65f2cdf44b1f433de9e083e4728f539ced52c
santos <sillasr61@gmail.com>
2025-07-23 23:46:04 -0300

    corrigido api rodando e mandando aquisicoes pelo isominia

--- src/main/java/org/example/main/RadarMdfApplication.java
+++ src/main/java/org/example/main/RadarMdfApplication.java
@@ -2,13 +2,14 @@ package org.example.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories(basePackages = "org.example.repository")
 @SpringBootApplication(scanBasePackages = "org.example")
+@EntityScan(basePackages = "org.example.produto")
+@EnableJpaRepositories(basePackages = "org.example.repository")
 public class RadarMdfApplication {
     public static void main(String[] args) {
         SpringApplication.run(RadarMdfApplication.class, args);
     }
 }
-
